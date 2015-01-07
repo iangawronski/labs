0dfeb58fbccca2a10a9279c120cec6d802032716
Ian Gawronski <iangawronski1@Ians-MacBook-Pro.local>
2015-01-07 14:07:12 -0500

    did the homework

--- 01-06/natohw.rb
+++ 01-06/natohw.rb
@@ -1,1 +1,36 @@
-na_aplha = {"a" => alpha, "b" => beta, "c" => charlie, "d" => delta, "e" => echo, "f" => foxtrot, "e" => echo, "g" => golf, "h" => hotel, "i" => india, "j" => juliett, "k" => kilo, "l" => lima, "m" => mike, "n" => november, "o" => oscar, "p" => papa, "q" => quebec, "r" => romeo, "s" => sierra, "t" => tango, "u" => uniform, "v" => victor, "w" => whiskey, "x" => xray, "y" => yankee, "z" => zulu}
+require 'pry'
+
+alphabet = { "a" => "alpha", "b" => "beta", "c" => "charlie", "d" => "delta", "e" => "echo", "f" => "foxtrot", "g" => "golf", "h" => "hotel", "i" => "india", "j" => "juliett", "k" => "kilo", "l" => "lima", "m" => "mike", "n" => "november", "o" => "oscar", "p" => "papa", "q" => "quebec", "r" => "romeo", "s" => "sierra", "t" => "tango", "u" => "uniform", "v" => "victor", "w" => "whiskey", "x" => "xray", "y" => "yankee", "z" => "zulu" }
+
+# puts alphabet.keys.inspect
+# puts alphabet.values.inspect
+=begin
+msg.each do |c|
+	puts alphabet[c]
+end
+=end
+puts "Do you want to encode or decode? E for encode, D for decode"
+option = gets.chomp!
+
+def encode(message, alphabet)
+  message.downcase!
+  message.each_char {|t| print alphabet[t], " "}
+end
+
+def decode(message, alphabet)
+  alphabetDecode = alphabet.invert
+  alphabetArray = message.split
+  alphabetArray.each do |t|
+    print alphabetArray[t]
+  end
+end
+
+msg = gets.chomp!
+
+if option == "E" || option == "e"
+	encode(msg, alphabet)
+elsif option == "D" || option == "d"
+	decode(msg, alphabet)
+end
+
+
